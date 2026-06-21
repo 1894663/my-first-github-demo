501c97a5295bc3bf4706d92688d9c0757bc85b9a
1894663 <1021277117@qq.com>
2026-06-21 13:54:48 +0800

    首版可运行Python测试代码

--- hello.py
+++ hello.py
@@ -1,16 +1,7 @@
 # hello.py
+def main():
+    print("Hello, GitHub!")
+    print("This is my first upload test.")
 
-  def main():
-      print("Hello, GitHub!")
-      print("This is my first upload test.")
-
-  if __name__ == "__main__":
-      main()
-
-  运行方式：
-
-  python hello.py
-
-  输出：
-  Hello, GitHub!
-  This is my first upload test.
+if __name__ == "__main__":
+    main()
